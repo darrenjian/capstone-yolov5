b7b9093bd591c32cd5e00c5ae4ef5c5de8415c6a
Darren Jian <dj2565@nyu.edu>
2025-11-11 20:10:18 -0500

    update hyperparameters

--- capstone_scripts/train_multigpu.sh
+++ capstone_scripts/train_multigpu.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --partition=a100_short
+#SBATCH --partition=gpu8_short
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=40
-#SBATCH --time=1-00:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem=200G
-#SBATCH --gres=gpu:a100:4
-#SBATCH --output=slurm_train_multigpu_%j.out
+#SBATCH --gres=gpu:a100:8
+#SBATCH --output=n_cls_05_meniscus.out
 
 # fail on error
 set -euo pipefail
 
 # go to repo root
-cd /gpfs/home/pb3060/capstone-yolov5
+cd /gpfs/home/dj2565/capstone-yolov5
 
 # load python module used interactively
 module load python/gpu/3.10.6-cuda12.9
@@ -32,16 +32,16 @@ fi
 # Multi-GPU training with DDP (Distributed Data Parallel)
 # Using 4 GPUs, batch can be increased proportionally
 python -m torch.distributed.run \
-    --nproc_per_node 4 \
+    --nproc_per_node 8 \
     --master_port 29500 \
     train.py \
     --img 640 \
     --batch 32 \
     --epochs 100 \
-    --data /gpfs/home/pb3060/capstone-yolov5/yolo_dataset/dataset.yaml \
-    --weights yolov5s.pt \
+    --data /gpfs/home/dj2565/capstone-yolov5/yolo_dataset/dataset.yaml \
+    --weights yolov5n.pt \
     --device 0,1,2,3 \
-    --name meniscus_yolov5s_multigpu_100ep_new_val_110625 \
+    --name n_cls_05_meniscus \
     --hyp ./data/hyps/hyp.scratch-low.yaml \
     --patience 50 \
     --save-period 10
